ab4443062382b1423664e9d228203c63657572c2
Kelli Frank <kdf219@lehigh.edu>
2017-09-29 14:29:32 -0400

    updated student.c file with group member names

--- cse303.p3/student.c
+++ cse303.p3/student.c
@@ -5,6 +5,6 @@
  */
 struct student_t student =
 {
-	"", /* member names, comma separated, e.g., Michael Spear */
-	""  /* member emails, comma separated, e.g., mfs409@lehigh.edu */
+	"Kelli Frank, Will Silverstone, Russel Laros", /* member names, comma separated, e.g., Michael Spear */
+	"kdf219, wes219, rkl218"  /* member emails, comma separated, e.g., mfs409@lehigh.edu */
 };
